7353b75370865a6e90b911f0fd90ffea0d345df7
Ant0nSk0 <anton.skovlund@gmail.com>
2022-09-25 04:52:28 +0000

    Add models and admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipe_post')
+    image = CloudinaryField('image', default='placeholder')
+    intro = models.TextField(blank=True)
+    ingredients = models.TextField()
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        User, related_name='recipe_like', blank=True)
+    favorite = models.ManyToManyField(
+        User, related_name='recipe_favorite', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
