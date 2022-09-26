3d60c5710631b4b50764a3540b9828b0a3b0cb7f
Ant0nSk0 <anton.skovlund@gmail.com>
2022-09-26 21:50:18 +0000

    Add view and templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Recipe
 
 # Create your views here.
+
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    template_name = 'index.html'
+    paginate_by = 6
+    ordering = ['-created_on']
