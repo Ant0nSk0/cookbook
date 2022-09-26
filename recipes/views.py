8007c127c2ba82f5b9fdb6b14f9217626e82071d
Ant0nSk0 <anton.skovlund@gmail.com>
2022-09-26 23:48:42 +0000

    Add authentication and templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -10,3 +11,25 @@ class RecipeList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
     ordering = ['-created_on']
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        recipe = get_object_or_404(Recipe, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm(),
+            },
+        )
